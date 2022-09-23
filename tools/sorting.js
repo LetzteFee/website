5fdce04aa14609c56a47f15b8189cf783b788ed8
LetzteFee <jannes.folkerts@protonmail.com>
2022-09-23 23:27:50 +0200

    added alpha sorting algorithms page

---
+++ tools/sorting.js
@@ -0,0 +1,17 @@
+function start_sorting(){
+    let arr = document.getElementById("content").innerHTML.split(" ").map(v => Number(v));
+    let str = bubble_sort(arr).join(" ");
+
+    document.getElementById("result").innerHTML = str;
+    doLog("start_sorting()", "finished")
+}
+
+function updateOutput(arr, i){
+    return
+    if(i % 100 != 0){return}
+    document.getElementById("result").innerHTML = arr.join(" ");
+    console.log(arr)
+}
+function getRandomInput(){
+    document.getElementById("content").innerHTML = getRandomArray(10000, 1000).join(" ");
+}
