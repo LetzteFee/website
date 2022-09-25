4f1cece06d8b84eb06bfe47c0eb96531ff79c452
LetzteFee <jannes.folkerts@protonmail.com>
2022-09-25 15:37:49 +0200

    refactoring

--- tools/sorting.js
+++ tools/sorting.js
@@ -1,6 +1,6 @@
 function start_sorting(){
     let arr = document.getElementById("content").innerHTML.split(" ").map(v => Number(v));
-    let str = bubble_sort(arr).join(" ");
+    let str = bubble_sort(arr, updateOutput).join(" ");
 
     document.getElementById("result").innerHTML = str;
     doLog("start_sorting()", "finished")
