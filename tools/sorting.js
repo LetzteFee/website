41be3fd221cb0b1a41220c7a885d67e020f7e76e
LetzteFee <jannes.folkerts@protonmail.com>
2022-10-28 00:35:13 +0200

    finished algorithm menu and  added selection sort

--- tools/sorting.js
+++ tools/sorting.js
@@ -1,10 +1,11 @@
 function start_sorting(){
     //gets called by button
     let input = document.getElementById("content").innerHTML;
-    doLog("start_sorting()", input);
+    //doLog("start_sorting()", input);
     input = input.split(" ").map(v => Number(v));
-    doLog("start_sorting()", input);
-    initSorting(input, updateOutput, 0);
+    //doLog("start_sorting()", input);
+    let algo = document.getElementById("select_algorithm").value;
+    initSorting(input, updateOutput, algo);
 
     doLog("start_sorting()", "finished");
 }
