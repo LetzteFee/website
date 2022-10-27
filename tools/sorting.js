3b985603efc9381d7c5603d8ffd1f02934b08e6c
LetzteFee <jannes.folkerts@protonmail.com>
2022-10-27 23:30:09 +0200

    prepared for multiples algos; made input readonly

--- tools/sorting.js
+++ tools/sorting.js
@@ -1,17 +1,18 @@
 function start_sorting(){
     //gets called by button
-    let arr = document.getElementById("content").innerHTML.split(" ").map(v => Number(v));
-    bubble_sort(arr, updateOutput);
+    let input = document.getElementById("content").innerHTML;
+    doLog("start_sorting()", input);
+    input = input.split(" ").map(v => Number(v));
+    doLog("start_sorting()", input);
+    initSorting(input, updateOutput, 0);
 
-    doLog("start_sorting()", "finished")
+    doLog("start_sorting()", "finished");
 }
 
-function updateOutput(arr, i){
-    return;
-    if(i % 100 != 0){return}
-    document.getElementById("result").innerHTML = arr.join(" ");
-    console.log(arr)
+function updateOutput(input){
+    document.getElementById("result").innerHTML = input.join(" ");
 }
 function getRandomInput(){
     document.getElementById("content").innerHTML = getRandomArray(100, 100).join(" ");
+    doLog("getRandomInput()", document.getElementById("content").innerHTML)
 }
