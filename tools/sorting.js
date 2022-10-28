03df4bfb7464ddf2cba9be93337116bc74945b16
LetzteFee <jannes.folkerts@protonmail.com>
2022-10-28 02:29:12 +0200

    added counter callback display function

--- tools/sorting.js
+++ tools/sorting.js
@@ -5,13 +5,16 @@ function start_sorting(){
     input = input.split(" ").map(v => Number(v));
     //doLog("start_sorting()", input);
     let algo = document.getElementById("select_algorithm").value;
-    initSorting(input, updateOutput, algo);
+    initSorting(input, updateOutput, updateCounter, algo);
 
     doLog("start_sorting()", "finished");
 }
 
 function updateOutput(input){
     document.getElementById("result").innerHTML = input.join(" ");
+}
+function updateCounter(input){
+    document.getElementById("counter").innerHTML = Number(input);
 }
 function getRandomInput(){
     document.getElementById("content").innerHTML = getRandomArray(100, 100).join(" ");
