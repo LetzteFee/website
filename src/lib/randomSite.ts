3fad01d273c4a12e019b96ec11d3d263d5bd09a7
LetzteFee <jannes.folkerts@protonmail.com>
2022-11-08 23:32:11 +0100

    random.ts: refactor

--- src/lib/randomSite.ts
+++ src/lib/randomSite.ts
@@ -1,33 +1,22 @@
-let possible_Sites_Animations = ["fall.html", "hexagon.html", "line.html", "matrix.html", "matrix_light.html", "selfmadecircles.html"];
-let possible_Sites_Games = ["duet.html", "tictactoe.html"];
-let possible_Sites_Tools = ["index.html"];
+function get_link(path_prefix: string = ''): string {
+    let possible_Sites_Animations: string[] = ["fall.html", "hexagon.html", "line.html", "matrix.html", "matrix_light.html", "selfmadecircles.html"];
+    let possible_Sites_Games: string[] = ["duet.html", "tictactoe.html"];
+    let possible_Sites_Tools: string[] = ["index.html", "otp.html", "pwd-gen.html", "sorting.html"];
 
-function get_link(path_prefix: string){
-    let possible_Sites = [];
-    for(let i = 0; i < possible_Sites_Animations.length; i++){
-        possible_Sites[possible_Sites.length] = path_prefix + "animations/" + possible_Sites_Animations[i];
-    }
-    for(let i = 0; i < possible_Sites_Games.length; i++){
-        possible_Sites[possible_Sites.length] = path_prefix + "games/" + possible_Sites_Games[i];
-    }
-    for(let i = 0; i < possible_Sites_Tools.length; i++){
-        possible_Sites[possible_Sites.length] = path_prefix + "tools/" + possible_Sites_Tools[i];
-    }
+    possible_Sites_Animations = possible_Sites_Animations.map(function (v: string): string { return "animations/" + v });
+    possible_Sites_Games = possible_Sites_Games.map(function (v: string): string { return "games/" + v });
+    possible_Sites_Tools = possible_Sites_Tools.map(function (v: string): string { return "tools/" + v });
+    let possible_Sites: string[] = possible_Sites_Animations.concat(possible_Sites_Games, possible_Sites_Tools);
 
-    doLog("randomSite.js", possible_Sites.join(""));
-
-    let choosen_Site = possible_Sites[getRandomInt(0, possible_Sites.length - 1)];
-    doLog("randomSite.js", choosen_Site + " was seletcted.")
-
-    return choosen_Site;
+    return path_prefix + possible_Sites[getRandomInt(0, possible_Sites.length - 1)];
 }
 
-function modify_a(path_prefix: string){
+function modify_a(path_prefix: string): void {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     document.getElementById("link_random").href = get_link(path_prefix);
 }
 
-function go_to_random_link(path_prefix: string){
+function go_to_random_link(path_prefix: string): void {
     window.location.href = get_link(path_prefix);
 }
