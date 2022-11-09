c2bde3609568f633c7f5fabc97133a4a6f80761e
LetzteFee <jannes.folkerts@protonmail.com>
2022-11-09 11:40:24 +0100

    base more typescript

--- src/lib/base.lib.ts
+++ src/lib/base.lib.ts
@@ -1,15 +1,15 @@
-function getRandomInt(min: number, max: number){
+function getRandomInt(min: number, max: number): number{
     if(min == max){
         return min;
     }
     return Math.floor(min + Math.random() * (max - min + 1));
 }
 
-function getRandomBoolean(){
+function getRandomBoolean(): boolean{
     return Boolean(Math.floor(Math.random() * 2));
 }
 
-function getRandomArray (n: number, maxValue: number) {
+function getRandomArray (n: number, maxValue: number): number[] {
     if(typeof n != "number"){
         n = getRandomInt(2, 100);
     }
@@ -23,30 +23,30 @@ function getRandomArray (n: number, maxValue: number) {
     return arr.map(() => Math.floor(Math.random() * (maxValue + 1)));
 }
 
-function removeCharsFromString(str: string, char: string){
+function removeCharsFromString(str: string, char: string): string{
     while(str.includes(char)){
         str = str.replace(char, "");
     }
     return str;
 }
 
-function doLog(origin="unknown origin", content="", throw_error=false){
+function doLog(origin="unknown origin", content: any="", throw_error: boolean=false): void{
     let time = new Date();
-    let str = "[" + time.getMinutes() + ":" + time.getSeconds() + "]" + "[" + origin + "] " + content;
+    let str: string = "[" + time.getMinutes() + ":" + time.getSeconds() + "]" + "[" + origin + "] " + content;
     if(throw_error){
         throw str;
     }
     console.log(str);
 }
 
-function sumTrueBools(args: boolean[]){
+function sumTrueBools(args: boolean[]): number{
     let a = 0;
     for(let i = 0; i < args.length;i++){
         a = a + Number(args[i]);
     }
     return a;
 }
-function activateStrictMode(){
+function activateStrictMode(): void{
     "use strict";
     doLog("base.lib.js", "Strict Mode activated");
 }
