4d8eda693b1886573ddf80687c542892b4a6323d
LetzteFee <jannes.folkerts@protonmail.com>
2022-11-09 21:08:31 +0100

    added logs for building process

--- build.sh
+++ build.sh
@@ -2,9 +2,16 @@
 
 #npm install
 
+echo "removing build directory"
 rm -r build
+
+echo "creating build directory"
 mkdir build
+
+echo "copying from src to build"
 cp -r src/* build/
+
+echo "compiling typescript"
 npx tsc
 
 #remove .ts files from build dir
