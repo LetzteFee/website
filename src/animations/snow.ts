450f33143e820ddba0b3a705a9922c9fece7910f
LetzteFee <jannes.folkerts@protonmail.com>
2022-11-11 14:50:11 +0100

    changed to ts; fixed it and change to oop

---
+++ src/animations/snow.ts
@@ -0,0 +1,64 @@
+class flake {
+  private readonly x: number;
+  private currentY: number;
+  private readonly targetY: number;
+  private readonly width: number;
+  private readonly height: number;
+  constructor(inp_target: number = null, inp_current: number = 0) {
+    this.width = getRandomInt(0, 20);
+    this.height = getRandomInt(0, 20);
+
+    // @ts-expect-error
+    this.x = getRandomInt(0 + this.width / 2, width - this.width / 2);
+    if (inp_target == null) {
+      // @ts-expect-error
+      this.targetY = getRandomInt(0 + this.height / 2, height - this.height / 2);
+    }else{
+      this.targetY = inp_target;
+    }
+    this.currentY = inp_current;
+  }
+  public move(): void {
+    this.currentY++;
+  }
+  private color(): number {
+    return Number(DARKMODE) * 255 - this.currentY / this.targetY * 255 * (1 - Number(!DARKMODE) * 2);
+  }
+  public check(): boolean {
+    return this.currentY > this.targetY;
+  }
+  public draw(): void {
+    // @ts-expect-error
+    fill(this.color());
+    // @ts-expect-error
+    ellipse(this.x, this.currentY, this.width, this.height);
+  }
+}
+let objekt: flake[] = [];
+let DARKMODE: boolean = false;
+
+function setup(): void {
+  // @ts-expect-error
+  createCanvas(windowWidth, windowHeight);
+  // @ts-expect-error
+  fill(255);
+  // @ts-expect-error
+  noStroke();
+  for (let i = 0; i < 256; i++) {
+    // @ts-expect-error
+    let h = getRandomInt(0, height);
+    objekt[i] = new flake(h, getRandomInt(0, h));
+  }
+}
+
+function draw() {
+  // @ts-expect-error
+  background(Number(!DARKMODE) * 255);
+  for (let i = 0; i < objekt.length; i++) {
+    objekt[i].move();
+    objekt[i].draw();
+    if (objekt[i].check()) {
+      objekt[i] = new flake();
+    }
+  }
+}
--- src/lib/base.lib.ts
+++ src/lib/base.lib.ts
@@ -1,19 +1,19 @@
-function getRandomInt(min: number, max: number): number{
-    if(min == max){
+function getRandomInt(min: number = 0, max: number = 100): number {
+    if (min >= max) {
         return min;
     }
     return Math.floor(min + Math.random() * (max - min + 1));
 }
 
-function getRandomBoolean(): boolean{
+function getRandomBoolean(): boolean {
     return Boolean(Math.floor(Math.random() * 2));
 }
 
-function getRandomArray (n: number, maxValue: number): number[] {
-    if(typeof n != "number"){
+function getRandomArray(n: number, maxValue: number): number[] {
+    if (typeof n != "number") {
         n = getRandomInt(2, 100);
     }
-    if(typeof maxValue != "number"){
+    if (typeof maxValue != "number") {
         maxValue = getRandomInt(0, 1000);
     }
 
@@ -23,30 +23,30 @@ function getRandomArray (n: number, maxValue: number): number[] {
     return arr.map(() => Math.floor(Math.random() * (maxValue + 1)));
 }
 
-function removeCharsFromString(str: string, char: string): string{
-    while(str.includes(char)){
+function removeCharsFromString(str: string, char: string): string {
+    while (str.includes(char)) {
         str = str.replace(char, "");
     }
     return str;
 }
 
-function doLog(origin="unknown origin", content: any="", throw_error: boolean=false): void{
+function doLog(origin = "unknown origin", content: any = "", throw_error: boolean = false): void {
     let time = new Date();
     let str: string = "[" + time.getMinutes() + ":" + time.getSeconds() + "]" + "[" + origin + "] " + content;
-    if(throw_error){
+    if (throw_error) {
         throw str;
     }
     console.log(str);
 }
 
-function sumTrueBools(args: boolean[]): number{
+function sumTrueBools(args: boolean[]): number {
     let a = 0;
-    for(let i = 0; i < args.length;i++){
+    for (let i = 0; i < args.length; i++) {
         a = a + Number(args[i]);
     }
     return a;
 }
-function activateStrictMode(): void{
+function activateStrictMode(): void {
     "use strict";
     doLog("base.lib.js", "Strict Mode activated");
 }
