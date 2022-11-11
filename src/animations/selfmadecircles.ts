90f284870467e398638fcf94be09c44288fc2712
LetzteFee <jannes.folkerts@protonmail.com>
2022-11-11 21:47:46 +0100

    perfected selfmade circles class

--- src/animations/selfmadecircles.ts
+++ src/animations/selfmadecircles.ts
@@ -1,21 +1,32 @@
+interface Color {
+  r: number,
+  g: number,
+  b: number,
+}
 class Circle {
-  private x: number;
-  private y: number;
-  private size: number;
-  private quality: number;
-  constructor(inp_x: number, inp_y: number, inp_size: number, inp_quality: number = 5) {
+  public x: number;
+  public y: number;
+  public size: number;
+  private readonly quality: number;
+  private color: Color;
+  constructor(inp_x: number, inp_y: number, inp_size: number, inp_quality: number = 5, inp_color: Color = { r: 0, g: 0, b: 0 }) {
     this.x = inp_x;
     this.y = inp_y;
     this.size = inp_size;
     this.quality = 5 / inp_quality;
+    this.color = inp_color;
   }
-  public draw(inp_type: number = 2) {
+  public draw(inp_type: number = 2): void {
+    //@ts-ignore
+    stroke(this.color.r, this.color.g, this.color.b)
+
     let deltaX: number;
     let deltaY: number;
     let x: number;
     let y: number = this.y;
     let r = this.size / 2;
-    for (let i = 0; i < this.size; i += this.quality) {
+    //horizontal rendering
+    for (let i: number = 0; i < this.size; i += this.quality) {
       deltaX = i - this.size / 2;
       x = this.x + deltaX;
       if (inp_type == 1 || inp_type == 2) {
@@ -23,49 +34,54 @@ class Circle {
         //@ts-expect-error
         point(x, y - deltaY);
       }
-      if(inp_type == 0 ||  inp_type == 2){
+      if (inp_type == 0 || inp_type == 2) {
         deltaY = Math.sqrt(r * r - deltaX * deltaX);
         //@ts-expect-error
         point(x, y + deltaY);
       }
     }
-  }
-}
-class circles {
-  private quality: number;
-  private circles: Circle[];
-  constructor(inp_quality: number = 5) {
-    this.quality = inp_quality;
-    this.circles = [];
-  }
-  public render() {
-    for (let i = 0; i < this.circles.length; i++) {
-      this.circles[i].draw();
+
+    x = this.x;
+    //vertical rendering
+    for (let i: number = 0; i < this.size; i++) {
+      deltaY = i - this.size / 2;
+      y = this.y + deltaY;
+      if (inp_type == 1 || inp_type == 2) {
+        deltaX = Math.sqrt(r * r - deltaY * deltaY);
+        //@ts-expect-error
+        point(x - deltaX, y);
+      }
+      if (inp_type == 0 || inp_type == 2) {
+        deltaX = Math.sqrt(r * r - deltaY * deltaY);
+        //@ts-expect-error
+        point(x + deltaX, y);
+      }
     }
   }
-  public addCircle(inp_x: number, inp_y: number, inp_size: number): number {
-    let id = this.circles.length;
-    this.circles[id] = new Circle(inp_x, inp_y, inp_size, this.quality);
-    return id;
-  }
 }
-let circles01: circles;
+
+let circles01: Circle[] = [];
 //@ts-ignore
-function setup(){
+function setup(): void {
   //@ts-expect-error
   createCanvas(windowWidth, windowHeight);
   //@ts-expect-error
   background(255);
-  circles01 = new circles();
+
+
   //@ts-expect-error
   let w: number = width;
   //@ts-expect-error
   let h: number = height;
-  for(let i = 0; i < 10; i++){
-    circles01.addCircle(getRandomInt(0, w), getRandomInt(0, h), getRandomInt(0, 100))
+  for (let i = 0; i < 20; i++) {
+    let color: Color = { r: getRandomInt(0, 255), g: getRandomInt(0, 255), b: getRandomInt(0, 255) };
+    circles01[i] = new Circle(getRandomInt(0, w), getRandomInt(0, h), getRandomInt(0, 100), 5, color)
   }
 }
 //@ts-ignore
-function draw(){
-  circles01.render();
+function draw(): void {
+  for (let i = 0; i < circles01.length; i++) {
+    circles01[i].draw();
+    circles01[i].size += getRandomInt(-10, 10);
+  }
 }
