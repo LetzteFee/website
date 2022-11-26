ac4332e4df4d657e8b84143b2a9df11238d1d8f4
LetzteFee <jannes.folkerts@protonmail.com>
2022-11-26 16:19:20 +0100

    added dev site to random sites list

--- src/lib/randomSite.ts
+++ src/lib/randomSite.ts
@@ -1,12 +1,43 @@
 function get_link(path_prefix: string = ''): string {
-    let possible_Sites_Animations: string[] = ["fall.html", "hexagon.html", "line.html", "matrix.html", "matrix_light.html", "selfmadecircles.html"];
-    let possible_Sites_Games: string[] = ["duet.html", "tictactoe.html"];
-    let possible_Sites_Tools: string[] = ["index.html", "otp.html", "pwd-gen.html", "sorting.html"];
+    let possible_Sites_Animations: string[] = [
+        "fall.html",
+        "hexagon.html",
+        "line.html",
+        "matrix.html",
+        "matrix_light.html",
+        "selfmadecircles.html",
+        "index.html"
+    ];
+    let possible_Sites_Games: string[] = [
+        "duet.html",
+        "tictactoe.html",
+        "index.html"
+    ];
+    let possible_Sites_Tools: string[] = [
+        "index.html",
+        "otp.html",
+        "pwd-gen.html",
+        "sorting.html"
+    ];
+    let possible_Sites_dev: string[] = [
+        "error404.html",
+        "template.html",
+        "test1.html",
+        "test2.html",
+        "test3.html",
+        "test4.html",
+        "test5.html"
+    ];
 
     possible_Sites_Animations = possible_Sites_Animations.map(function (v: string): string { return "animations/" + v });
     possible_Sites_Games = possible_Sites_Games.map(function (v: string): string { return "games/" + v });
     possible_Sites_Tools = possible_Sites_Tools.map(function (v: string): string { return "tools/" + v });
-    let possible_Sites: string[] = possible_Sites_Animations.concat(possible_Sites_Games, possible_Sites_Tools);
+    possible_Sites_dev = possible_Sites_dev.map(function (v: string): string { return "dev/" + v });
+    let possible_Sites: string[] = possible_Sites_Animations.concat(
+        possible_Sites_Games,
+        possible_Sites_Tools,
+        possible_Sites_dev
+    );
 
     return path_prefix + possible_Sites[getRandomInt(0, possible_Sites.length - 1)];
 }
