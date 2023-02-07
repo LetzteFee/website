17bc3c19c2477a90311a5e42e275e5cee5260403
LetzteFee <jannes.folkerts@protonmail.com>
2023-02-07 21:10:12 +0100

    removed npm

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Installing missing Node Modules"
-npm install
-
-echo "Downloading p5 lib"
-wget https://github.com/processing/p5.js/releases/download/v1.5.0/p5.js
-echo "Installing p5 lib"
-mkdir src/p5
-mv p5.js src/p5/
