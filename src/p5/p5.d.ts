57cfda8ff1d40ac3df7089feb051f697ceabda73
LetzteFee <107996162+LetzteFee@users.noreply.github.com>
2023-10-02 14:43:05 +0200

    removed canvas libs to better integrate with ts

---
+++ src/p5/p5.d.ts
@@ -0,0 +1,17 @@
+declare const windowWidth: number;
+declare const windowHeight: number;
+declare const width: number;
+declare const height: number;
+declare function createCanvas(n1: number, n2: number): void;
+declare function background(n: number, m?: number, o?: number): void;
+declare function noStroke(): void;
+declare function stroke(n: number, n1?: number, n2?: number): void;
+declare function ellipse(x: number, y: number, d: number, d1?: number): void;
+declare function noFill(): void;
+declare function fill(r: number, b?: number, g?: number): void;
+declare function point(x: number, y: number): void;
+declare function random(r1: number, r2?: number, r3?: number): number;
+declare function createInput(value: string, type: string): any;
+declare function strokeWeight(n: number): void;
+declare function text(t: any, x: number, y: number): void;
+declare function line(x1: number, y1: number, x2: number, y2: number): void;
