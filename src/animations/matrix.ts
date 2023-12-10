012c969c82ea5b138c5af6ad50517318a8f8d712
LetzteFee <107996162+LetzteFee@users.noreply.github.com>
2023-12-10 21:23:32 +0100

    ported duet_opp to ts

--- src/animations/matrix.ts
+++ src/animations/matrix.ts
@@ -1,79 +1,79 @@
 class MatrixHeavySymbol {
-  private char: string = MatrixHeavySymbol.genChar();
-  private alpha: number = random(32, 255);
-  private static genChar(): string {
-    let rangeMin = 0x3400;
-    let rangeMax = 0x4dbf;
-    return String.fromCharCode(random(rangeMin, rangeMax));
-  }
-  public draw(x: number, y: number): void {
-    stroke(0, 128, 0, this.alpha);
-    fill(146, 232, 185, this.alpha);
-    text(this.char, x, y);
-  }
-  public reset(): void {
-    this.char = MatrixHeavySymbol.genChar();
-    this.alpha = random(32, 255);
-  }
+    private char: string = MatrixHeavySymbol.genChar();
+    private alpha: number = random(32, 255);
+    private static readonly rangeMin: number = 0x3400;
+    private static readonly rangeMax: number = 0x4dbf;
+    private static genChar(): string {
+        return String.fromCharCode(random(MatrixHeavySymbol.rangeMin, MatrixHeavySymbol.rangeMax));
+    }
+    public draw(x: number, y: number): void {
+        stroke(0, 128, 0, this.alpha);
+        fill(146, 232, 185, this.alpha);
+        text(this.char, x, y);
+    }
+    public reset(): void {
+        this.char = MatrixHeavySymbol.genChar();
+        this.alpha = random(32, 255);
+    }
 }
 class MatrixHeavyString {
-  private string: MatrixHeavySymbol[] = [];
-  private x: number = random(width);
-  private y: number = random(height);
-  private speed: number = random(1, 5);
-  constructor() {
-    let length = random(4, 15);
-    for (let i = 0; i < length; i++) {
-      this.string.push(new MatrixHeavySymbol());
+    private static readonly lineDistance: number = 18;
+    private string: MatrixHeavySymbol[] = [];
+    private x: number = random(width);
+    private y: number = random(height);
+    private speed: number = random(1, 5);
+    constructor() {
+        let length = random(4, 15);
+        for (let i: number = 0; i < length; i++) {
+            this.string.push(new MatrixHeavySymbol());
+        }
     }
-  }
-  public run(): void {
-    this.draw();
-    this.move();
-    this.newLetter();
-  }
-  private draw(): void {
-    let lineDistance = 18;
-    for (let i = 0; i < this.string.length; i++) {
-      this.string[i].draw(this.x, this.y + i * lineDistance);
+    public run(): void {
+        this.draw();
+        this.move();
+        this.newLetter();
     }
-  }
-  private move(): void {
-    this.y += this.speed;
-    if (this.y > height) {
-      this.reset();
+    private draw(): void {
+        for (let i: number = 0; i < this.string.length; i++) {
+            this.string[i].draw(this.x, this.y + i * MatrixHeavyString.lineDistance);
+        }
     }
-  }
-  private reset(): void {
-    this.x = random(width);
-    this.y = 0;
-    this.speed = random(1, 5);
+    private move(): void {
+        this.y += this.speed;
+        if (this.y > height) {
+            this.reset();
+        }
+    }
+    private reset(): void {
+        this.x = random(width);
+        this.y = 0 - this.string.length * MatrixHeavyString.lineDistance;
+        this.speed = random(1, 5);
 
-    for (let i = 0; i < this.string.length; i++) {
-      this.string[i].reset();
+        for (let i: number = 0; i < this.string.length; i++) {
+            this.string[i].reset();
+        }
     }
-  }
-  private newLetter(): void {
-    if (Math.round(random(0, 10)) == 0) {
-      this.string[Math.floor(random(this.string.length))].reset();
+    private newLetter(): void {
+        if (Math.round(random(0, 10)) == 0) {
+            this.string[Math.floor(random(this.string.length))].reset();
+        }
     }
-  }
 }
 
 let matrixHeavy: MatrixHeavyString[] = [];
 
 var setup = function () {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 
-  let numberOfStrings = width / 50;
-  for (let i = 0; i < numberOfStrings; i++) {
-    matrixHeavy.push(new MatrixHeavyString());
-  }
+    let numberOfStrings = width / 50;
+    for (let i: number = 0; i < numberOfStrings; i++) {
+        matrixHeavy.push(new MatrixHeavyString());
+    }
 };
 
 var draw = function () {
-  background(0);
-  for (let i = 0; i < matrixHeavy.length; i++) {
-    matrixHeavy[i].run();
-  }
+    background(0);
+    for (let i: number = 0; i < matrixHeavy.length; i++) {
+        matrixHeavy[i].run();
+    }
 };
--- src/p5/p5.d.ts
+++ src/p5/p5.d.ts
@@ -2,23 +2,26 @@ declare const windowWidth: number;
 declare const windowHeight: number;
 declare const width: number;
 declare const height: number;
+declare const PI: number;
 declare function resizeCanvas(w: number, h: number): void;
 declare function createCanvas(n1: number, n2: number): void;
 declare function frameRate(fps: number): void;
 declare function background(
-  r: number,
-  g?: number,
-  b?: number,
-  alpha?: number,
+    r: number,
+    g?: number,
+    b?: number,
+    alpha?: number,
 ): void;
 declare function noStroke(): void;
 declare function stroke(n: number, n1?: number, n2?: number, n3?: number): void;
 declare function ellipse(x: number, y: number, d: number, d1?: number): void;
+declare function rect(x: number, y: number, width: number, height?: number): void;
 declare function noFill(): void;
 declare function fill(r: number, b?: number, g?: number, alpha?: number): void;
 declare function point(x: number, y: number): void;
 declare function random(r1: number, r2?: number, r3?: number): number;
 declare function noise(n: number): number;
+declare function sin(alpha: number): number;
 declare function createInput(value: string, type: string): any;
 declare function strokeWeight(n: number): void;
 declare function text(t: any, x: number, y: number): void;
